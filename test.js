0f2f845a3cfb5d0ce76344be60a7dc651ebc13eb
Daniel Lowe <dnllowe@yahoo.com>
2017-03-17 17:35:57 -0400

    feat(address book): Should be able to load the address book from any Mac without hardcoding the path

--- test.js
+++ test.js
@@ -1,5 +1,16 @@
 const iMessage = require('imessage');
+const AddressBook = require('./AddressBook');
+
+const ab = new AddressBook();
 const im = new iMessage();
-im.getMessages("Haha", (error, messages) => {
-  console.log(messages);
-});
+
+console.log('AB', ab);
+console.log('IM', im);
+
+// im.getMessages(false, true, (error, messages) => {
+//   console.log(messages);
+// });
+
+ab.getContacts((error, contacts) => {
+  console.log(contacts);
+})
