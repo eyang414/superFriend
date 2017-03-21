3519f136d49d5a29054a69fb30e975f56af8746e
Eric Yang <ericswyang@gmail.com>
2017-03-21 11:07:52 -0400

    feat(seed Users) alex is our user with contacts. All you guys are also users, check out dummyData.js

--- db/models/message.js
+++ db/models/message.js
@@ -6,18 +6,7 @@ const db = require('APP/db')
 const Message = db.define('message', {
   content: {
     type: Sequelize.TEXT
-  },
-  senderId: {
-    type: Sequelize.INTEGER,
-    allowNull: false
-  },
-  recipientId: {
-    type: Sequelize.INTEGER,
-    allowNull: false
   }
 })
 
 module.exports = Message
-
-
-
--- db/seed.js
+++ db/seed.js
@@ -1,26 +1,90 @@
 'use strict'; // eslint-disable-line semi
 
 const db = require('APP/db')
+const User = require('./models/user')
+const Message = require('./models/message')
+const Promise = require('bluebird')
 
 const dummyUser = require('../dummyData').dummyUser;
 const dummyMessages = require('../dummyData').dummyMessages;
 
 const seedUsers = () => db.Promise.map(dummyUser, user => db.model('users').create(user))
-const seedMessages = () => db.Promise.map(dummyMessages, messages => db.model('message').create(message))
+const seedMessages = () => db.Promise.map(dummyMessages, messages => db.model('message').create(messages))
+
+//TODO: need to change dummyseed data to NOT have recipient and sender in messages. need to set recipient, sender, friend IDs
+
+//not sure why the below method isnt working. getting error: relation "users" does not exist
+// db.sync({force: true})
+//   .then( () => (User.bulkCreate(dummyUser)) )
+//   .then(() => (User.findById(1)))
+//   .then(user => {
+//     for (let i = 2; i <= 25; i++) {
+//       user.addFriend(i)
+//     }
+//     return user.save()
+//   })
+//   .catch(error => console.error(error))
+//   .finally(() => db.close())
 
 db.didSync
   .then(() => db.sync({force: true}))
-  .then(seedUsers)
-  .then(users => {
-    console.log(`Seeded ${users.length} users OK`)
-    for (let i=2; i<=25; i++){
-      users[0].addFriend(i)
-    }
-    return users[0].save()
+  .then( () => Promise.all([seedUsers(), seedMessages()]) )
+  .then((seededStuff) => {
+
+    User.findOne({
+      where: {username: 'ak123'}
+    })
+    .then( (ourUser) => {
+      console.log(ourUser)
+
+      for (let i=0; i<=29; i++){
+        ourUser.addFriend(i)
+      }
+    })
+
+
+
+    //made it so that you AND all your contacts are your friends
+
+
+    // for (let i=1; i<=50; i++){
+    //   seededStuff[1].addSender(1);
+    //   seededStuff[1].addRecipient(i)
+    // }
+    //
+    // for (let i=51; i<=100; i++){
+    //   seededStuff[1].addRecipient()
+    // }
+
   })
   .catch(error => console.error(error))
-  .finally(() => db.close())
-// 
+
+  //   (seedUsers, seedMessages) => Promise.all)
+  //
+  // .then((messages) => {
+  //   console.log(messages)
+  //   for (let i=2; i<=25; i++){
+  //     users[0].addFriend(i)
+  //   }
+  //   return users[0].save()
+  // })
+  // .catch(error => console.error(error))
+  // .finally(() => db.close())
+
+
+// db.didSync
+//   .then(() => db.sync({force: true}))
+//   .then(seedUsers, seedMessages)
+//   .then(stuff => {
+//     console.log(`Seeded ${users.length} users OK`)
+//     for (let i=2; i<=25; i++){
+//       users[0].addFriend(i)
+//     }
+//     return users[0].save()
+//   })
+//   .catch(error => console.error(error))
+//   .finally(() => db.close())
+//
 // db.didSync
 //   .then(() => db.sync({force: true}))
 //   .then(seedMessages)
