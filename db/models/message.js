cfc1dc251d4183915eb923528eb2601d217e4de6
Eric Yang <ericswyang@gmail.com>
2017-03-22 11:28:54 -0400

    fix(message) included ZFULLNUMBER a.k.a. phone number and adapted dummy data

--- db/models/message.js
+++ db/models/message.js
@@ -6,6 +6,12 @@ const db = require('APP/db')
 const Message = db.define('message', {
   content: {
     type: Sequelize.TEXT
+  },
+  ZFULLNUMBER: {
+    type: Sequelize.STRING,
+    set: function(number) {
+      this.setDataValue('ZFULLNUMBER', parseInt(number.replace(/[^0-9]/g, ''), 10).toString().slice(-10))
+      }
   }
 }, {
     classMethods: {
